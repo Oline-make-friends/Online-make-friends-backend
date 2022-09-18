445c8e922c8dd48b53a37ae8757d6fca5731192b
ThienBao <thienbaodx2000@gmail.com>
2022-09-18 19:44:35 +0700

    update user routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
 const userController = require("../controller/userController");
 
+//get All User
 router.get("/getAll", userController.getAllUser);
 
+//get All User by Fullname
 router.post("/getUserByFullName", userController.getUserByFullName);
+
+//update User
+router.post("/update", userController.updateUserProfile);
+
+//delete User
+router.post("/delete", userController.deleteUser)
+
+//add Friend
+router.post("/addFriend", userController.addFriends);
+//delete Friend
+router.post("/deleteFriend", userController.deleteFriends);
 
 module.exports = router;
