42f2c3a4902dae743e80ac68ee59cb4d89ebc4bb
mklaicogido123 <mklaicogido123@gmail.com>
2022-10-17 07:21:35 +0700

    old code

--- controller/userController.js
+++ controller/userController.js
@@ -292,8 +292,7 @@ const userController = {
         sender_id: req.body.sender_id,
         receiver_id: req.body.receiver_id,
       });
-      console.log(check);
-      if (friend.friends.includes(req.body.sender_id) || check === null) {
+      if (friend.friends.includes(req.body.receiver_id) || check === null) {
         // await friend.updateOne({ $pull: { friends_request: req.body._id } });
         return res.status(200).json("You already request this friend!");
       }
